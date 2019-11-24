48f40ffdcde2f17b18cec9600882135ad8c97e84
Pamela Wang <wangyeelin@gmail.com>
2019-11-24 22:03:11 +0800

    Misc Files

---
+++ 0_checkingDirectory.py
@@ -0,0 +1,11 @@
+import os
+import string
+from pathlib import Path
+from unidecode import unidecode
+import re
+from string import punctuation
+import youtube_dl
+
+
+if __name__ == "__main__":
+    run()
